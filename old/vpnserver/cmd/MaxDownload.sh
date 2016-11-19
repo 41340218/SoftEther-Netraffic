965ba177c545ae8a6386e848aadf84db6884d671
41340218 <15311449327@163.com>
2016-11-19 20:38:39 +0800

    Update MaxDownload.sh
    $passwd根据需要添加
    这样应该效率更高点
    我也是准备写个这个流控，看来你已经解决了，膜拜
    其他文件也可以用类似代码执行

--- old/vpnserver/cmd/MaxDownload.sh
+++ old/vpnserver/cmd/MaxDownload.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cd /vpnserver
-./vpncmd <<EOF
-1
 
-DEFAULT
-UserPolicySet
-$1
-MaxDownload
-$2
-exit
-EOF
+./vpncmd /server localhost /password:$passwd /adminhub:default /cmd UserPolicySet $1 /NAME:MaxDownload /VALUE:$2
+
+
+#./vpncmd <<EOF
+#1
+#
+#DEFAULT
+#UserPolicySet
+#$1
+#MaxDownload
+#$2
+#exit
+#EOF
